3afc27c013302043ac29f91e35c0311d26063de1
lokisassone-rgb <lokisassone@gmail.com>
2026-01-15 21:08:58 -0800

    update conv_gold_tiled

--- cpp/conv_gold_tiled.cpp
+++ cpp/conv_gold_tiled.cpp
@@ -23,6 +23,25 @@ void conv_gold_tiled(
   // will use this function to verify the output of your accelerator.
  
   // Your code starts here
+  memset(ofmap, 0, sizeof(int32_t)*OY1*OY0*OX1*OX0*OC1*OC0); //initialize ofmap to zero
 
-  // Your code ends here
+  for (int oy1 = 0; oy1 < OY1; oy1++) {
+    for (int ox1 = 0; ox1 < OX1; ox1++) {
+      for (int oc1 = 0; oc1 < OC1; oc1++) {
+        for (int ic1 = 0; ic1 < IC1; ic1++) {
+          for (int fy = 0; fy < FY; fy++) {
+            for (int fx = 0; fx < FX; fx++) {
+              for (int oy0 = 0; oy0 < OY0; oy0++) {
+                for (int ox0 = 0; ox0 < OX0; ox0++) {
+                  for (int oc0 = 0; oc0 < OC0; oc0++) {
+                    int oy = oy1*OY0 + oy0;
+                    int ox = ox1*OX0 + ox0;
+                    int oc = oc1*OC0 + oc0;
+                    for (int ic0 = 0; ic0 < IC0; ic0++) {
+                      int ic = ic1*IC0 + ic0;
+                      ofmap[oy][ox][oc] += (int32_t)
+                      ifmap[STRIDE*oy+fy][STRIDE*ox+fx][ic] *
+                      (int32_t) weights[fy][fx][ic][oc];
+                      } } } } } } } } } }
+ 
 }
